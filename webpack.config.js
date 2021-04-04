1c8756f71300866cdc97b5aedf7ba68a51680495
Valentin Chanas <anisometropie@gmail.com>
2021-04-04 03:03:59 +0200

    base config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules=true']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader?name=/[name].[ext]',
+          options: {
+            publicPath: '/',
+            outputPath: '',
+            name: '[name].[ext]'
+          }
+        },
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000,
+  },
+  mode: 'development'
+}
