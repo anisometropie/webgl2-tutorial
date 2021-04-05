f8140b51de6727e556d3924b756271303f5d6e15
Valentin Chanas <anisometropie@gmail.com>
2021-04-05 02:42:39 +0200

    meaningful refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { createShader, createProgram } from './webGl'
+import { 
+  createShader, 
+  createProgram, 
+  clearCanvas,
+  createAndBindBuffer,
+  createVertexArray,
+ } from './webGl'
 
 import vertexShaderSource from './vertex.glsl'
 import fragmentShaderSource from './fragment.glsl'
@@ -14,36 +20,29 @@ const program = createProgram(gl, vertexShader, fragmentShader);
 const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+// we first create an array buffer, that is going to feed data to the vertex array
+const positionBuffer = createAndBindBuffer(gl, gl.ARRAY_BUFFER);
 const positions = [
   0, 0,
-  400, 300,
-  0, 300,
-  200, 20,
-  200, 60,
-  100, 60,
+  100, 100,
+  100, 0
 ];
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-const vao = gl.createVertexArray();
-gl.bindVertexArray(vao);
-gl.enableVertexAttribArray(positionAttributeLocation);
-const size = 2;          // 2 components per iteration
-const type = gl.FLOAT;   // the data is 32bit floats
-const normalize = false; // don't normalize the data
-const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-const offset = 0;        // start at the beginning of the buffer
-gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+createVertexArray(
+  gl, 
+  {
+    attributeLocation: positionAttributeLocation,
+    size: 2,        // 2 components per iteration
+    type: gl.FLOAT, // the data is 32bit floats
+  },
+)
 
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 // Clear the canvas
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+clearCanvas(gl);
 
 gl.useProgram(program);
-
 gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-
-gl.drawArrays(gl.TRIANGLES, 0, 6);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
--- src/webGl.js
+++ src/webGl.js
@@ -24,3 +24,31 @@ export function createProgram(gl, vertexShader, fragmentShader) {
   console.error(gl.getProgramInfoLog(program));
   gl.deleteProgram(program);
 }
+
+export function clearCanvas(gl) {
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
+export function createAndBindBuffer(gl, glBuffer) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(glBuffer, buffer);
+  return buffer;
+}
+
+export function createVertexArray(
+  gl, 
+  {
+    attributeLocation,
+    size,
+    type,
+    normalize = false,
+    stride = 0,
+    offset = 0,
+  }
+  ) {
+  const vertexArray = gl.createVertexArray();
+  gl.bindVertexArray(vertexArray);
+  gl.enableVertexAttribArray(attributeLocation); // the data is 32bit floats      // start at the beginning of the buffer
+  gl.vertexAttribPointer(attributeLocation, size, type, normalize, stride, offset)
+}
